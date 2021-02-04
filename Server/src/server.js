2045c6034416f83930318e21ba1d34e9df70e456
glowing713 <glowing713@naver.com>
2021-02-05 00:15:59 +0900

    feat: Add express server
    express.js를 사용하여 로컬 서버 구성

---
+++ Server/src/server.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import express, { response } from "express";
+
+dotenv.config(); // 현재 directory 내의 .env 파일을 인식해서 환경변수를 설정해준다.
+const app = express();
+const port = process.env.PORT || 4000;
+
+// get 메소드를 사용하여 엔드포인트 작성
+app.get("/", (request, response) => {
+  response.json({
+    name: "sean",
+    status: "test",
+    feel: "yaaaaaay",
+    hunger: "quite hungry",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`🥳 Server is listening at http://localhost:${port}`);
+});
